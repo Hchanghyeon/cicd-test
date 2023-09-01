556abe3780ea25d5eea0ae633ee15ad129305f31
changhyeon <changhyeon.h@kakao.com>
2023-09-01 23:44:36 +0900

    test: 테스트 파일 변경

--- src/test/java/com/test/cicdtest/TestCiCd.java
+++ src/test/java/com/test/cicdtest/TestCiCd.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CicdTestApplicationTests {
+public class TestCiCd {
 
 	@Test
-	void contextLoads() {
+	private void test(){
+		System.out.println("test");
 	}
-
 }
